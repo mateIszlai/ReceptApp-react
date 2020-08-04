7694e2378d8a86f3b3a3cde661fedd07016b4ecd
Mate Iszlai <iszimate@gmail.com>
2020-08-04 12:09:50 +0200

    Add nav to homepage

--- receptapp/src/components/Navigation/Navigation.js
+++ receptapp/src/components/Navigation/Navigation.js
@@ -1,5 +1,23 @@
 import React, { Fragment } from "react";
+import { NavLink } from "react-router-dom";
+import HomeIcon from "@material-ui/icons/Home";
+import { IconButton } from "@material-ui/core";
 
 export default function Navigation() {
-    return <Fragment></Fragment>;
+    return (
+        <nav>
+            <div className="logo"></div>
+            <ul className="nav-links nav-links-left">
+                <li>
+                    <div className="link-wrapper">
+                        <NavLink activeClassName="is-active" to={"/"} exact>
+                            <IconButton aria-label="nav-to-home">
+                                <HomeIcon />
+                            </IconButton>
+                        </NavLink>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    );
 }
