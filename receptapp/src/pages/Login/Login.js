d05f5f6c52d5bdde60690257c7bb5291852d85ff
Máté Iszlai <iszimate@gmail.com>
2020-08-07 21:08:05 +0200

    Create Login.js

---
+++ receptapp/src/pages/Login/Login.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import { TextField } from '@material-ui/core'
+
+export default function Login() {
+    return (
+        <Fragment>
+            <div className="page-container">
+
+            </div>
+        </Fragment>
+    )
+}
