d8f0cc9f7d8308be9255f477d9be6d09322080a9
Máté Iszlai <iszimate@gmail.com>
2020-08-09 19:47:49 +0200

    Add userProvider to App

--- receptapp/src/App.js
+++ receptapp/src/App.js
@@ -6,20 +6,23 @@ import history from "./history";
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
+import { UserProvider } from "./context/USerContext";
 
 function App(props) {
-    return (
-        <div className="App">
-            <Router history={history}>
-                <Container>
-                    <Switch>
-                        <Route path="/" exact component={Home} />
-                        <Route path="/register" exact component={Register} />
-                    </Switch>
-                </Container>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Router history={history}>
+        <Container>
+          <Switch>
+            <UserProvider>
+              <Route path="/" exact component={Home} />
+              <Route path="/register" exact component={Register} />
+            </UserProvider>
+          </Switch>
+        </Container>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
