4d820d94a0cf3ca7f0097557003a9502f589a5ee
Máté Iszlai <iszimate@gmail.com>
2020-08-10 12:18:17 +0200

    Add navigation to header

--- receptapp/src/layout/Header.js
+++ receptapp/src/layout/Header.js
@@ -1,9 +1,10 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
+import Navigation from "../components/Navigation/Navigation";
 
 export default function Header() {
-    return (
-        <Fragment>
-            
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <Navigation />
+    </Fragment>
+  );
 }
