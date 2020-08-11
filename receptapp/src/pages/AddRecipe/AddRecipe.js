746d7e939dd4b90f0073efd5aa33158d2a576b96
Máté Iszlai <iszimate@gmail.com>
2020-08-11 16:24:10 +0200

    Add Recipe name input field

--- receptapp/src/pages/AddRecipe/AddRecipe.js
+++ receptapp/src/pages/AddRecipe/AddRecipe.js
@@ -1,9 +1,27 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import { Box, TextField } from "@material-ui/core";
 
 export default function AddRecipe() {
+  const [recipeName, setRecipeName] = useState("");
+
   return (
     <Fragment>
-      <div className="page-container"></div>
+      <div className="page-container">
+        <form noValidate autoComplete="off">
+          <Box className="textfield-container">
+            <TextField
+              className="textfield"
+              id="name-input"
+              variant="outlined"
+              label="Name of the recipe"
+              required
+              onChange={(e) => {
+                setRecipeName(e.target.value);
+              }}
+            />
+          </Box>
+        </form>
+      </div>
     </Fragment>
   );
 }
