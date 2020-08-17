4255ef9e8504b50184750fdac8b3259c4782f622
Máté Iszlai <iszimate@gmail.com>
2020-08-17 14:40:30 +0200

    Add card, need fix the images

--- receptapp/src/components/MiniRecipe/MiniRecipe.js
+++ receptapp/src/components/MiniRecipe/MiniRecipe.js
@@ -1,5 +1,20 @@
 import React, { Fragment } from "react";
+import { Card, CardMedia, CardContent } from "@material-ui/core";
 
-export default function MiniRecipe() {
-  return <Fragment></Fragment>;
+export default function MiniRecipe(props) {
+  const image = `data:image/png;base64,${props.mainPicture}`;
+  return (
+    <Fragment>
+      <Card className="minirecipe-card">
+        <CardMedia
+          className="minirecipe-main-picture"
+          title={props.name}
+          src={{ uri: image }}
+        />
+        <CardContent>
+          <p>{props.name}</p>
+        </CardContent>
+      </Card>
+    </Fragment>
+  );
 }
