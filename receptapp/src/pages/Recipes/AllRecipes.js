55b64d8def58e3107a0cabf5f2aa11cca9268983
Máté Iszlai <iszimate@gmail.com>
2020-08-17 14:41:23 +0200

    Map minirecipes

--- receptapp/src/pages/Recipes/AllRecipes.js
+++ receptapp/src/pages/Recipes/AllRecipes.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect, useState } from "react";
 import axios from "../../axios/axios";
 import { Box } from "@material-ui/core";
+import MiniRecipe from "../../components/MiniRecipe/MiniRecipe";
+import { NavLink } from "react-router-dom";
 
 export default function AllRecipes() {
   const [recipes, setRecipes] = useState([]);
@@ -20,7 +22,15 @@ export default function AllRecipes() {
   ) : (
     <Fragment>
       <div id="recipes-page-container">
-        <Box id="recipes-container"></Box>
+        <Box id="recipes-container">
+          {recipes.map((recipe) => (
+            <Box key={recipe.id} className="minirecipe-container">
+              <NavLink to={`/recipes/${recipe.id}`}>
+                <MiniRecipe name={recipe.name} />
+              </NavLink>
+            </Box>
+          ))}
+        </Box>
       </div>
     </Fragment>
   );
