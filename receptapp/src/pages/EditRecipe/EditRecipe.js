b5bec9c507862dbf0296167c3e8b5d5b2cd3fe80
Máté Iszlai <iszimate@gmail.com>
2020-08-25 12:52:46 +0200

    Add old values

--- receptapp/src/pages/EditRecipe/EditRecipe.js
+++ receptapp/src/pages/EditRecipe/EditRecipe.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState, useContext } from "react";
 import axios from "../../axios/axios";
 import { useParams } from "react-router";
 import { Box, Button } from "@material-ui/core";
@@ -13,21 +13,30 @@ import CookTimeInput from "../../components/TimeInputs/CookTimeInput";
 import AdditionalTimeInput from "../../components/TimeInputs/AdditionalTimeInput";
 import MessageNavDialog from "../../components/Dialogs/MessageNavDialog";
 import SmallDescriptionInput from "../../components/SmallDescriptionInput";
+import "./EditRecipe.css";
+import { UserContext } from "../../context/UserContext";
 
 export default function EditRecipe() {
   const { recipeId } = useParams();
   const [recipeName, setRecipeName] = useState("");
+  const [oldRecipeName, setOldRecipeName] = useState("");
   const [ingredients, setIngredients] = useState([]);
   const [description, setDescription] = useState([]);
   const [servings, setServings] = useState(0);
+  const [oldServings, setOldServings] = useState(0);
   const [preparationTimeAmount, setPreparationTimeAmount] = useState(0.0);
   const [preparationTimeUnit, setPreparationTimeUnit] = useState("");
+  const [oldPreparationTime, setOldPreparationTime] = useState("");
   const [cookTimeAmount, setCookTimeAmount] = useState(0.0);
   const [cookTimeUnit, setCookTimeUnit] = useState("");
+  const [oldCookTime, setOldCookTime] = useState("");
   const [additionalTimeAmount, setAdditionalTimeAmount] = useState(0.0);
   const [additionalTimeUnit, setAdditionalTimeUnit] = useState("");
+  const [oldAdditionalTime, setOldAdditionalTime] = useState("");
   const [smallDescription, setSmallDescription] = useState("");
+  const [oldSmallDescription, setOldSmallDescription] = useState("");
   const [show, setShow] = useState(false);
+  const user = useContext(UserContext)[0];
 
   const tryEditRecipe = () => {
     axios
@@ -51,31 +60,47 @@ export default function EditRecipe() {
 
   useEffect(() => {
     axios.get(`/Recipes/${recipeId}`).then((response) => {
+      setOldRecipeName(response.data.name);
       setRecipeName(response.data.name);
       setIngredients(response.data.ingredients);
       setDescription(response.data.description);
+      setOldServings(response.data.servings);
       setServings(response.data.servings);
       setSmallDescription(response.data.smallDescription);
+      setOldSmallDescription(response.data.smallDescription);
+      setOldPreparationTime(
+        `${response.data.preparationTimeAmount} ${response.data.preparationTimeUnit}`
+      );
       setPreparationTimeAmount(response.data.preparationTimeAmount);
       setPreparationTimeUnit(response.data.preparationTimeUnit);
       setCookTimeAmount(response.data.cookTimeAmount);
       setCookTimeUnit(response.data.cookTimeUnit);
+      setOldCookTime(
+        `${response.data.cookTimeAmount} ${response.data.cookTimeUnit}`
+      );
       setAdditionalTimeAmount(response.data.additionalTimeAmount);
       setAdditionalTimeUnit(response.data.additionalTimeUnit);
+      setOldAdditionalTime(
+        `${response.data.additionalTimeAmount} ${response.data.additionalTimeUnit}`
+      );
     });
   }, [recipeId]);
 
-  return (
+  return !user.loggedIn ? (
+    <Fragment>
+      <h2>Please login to this action</h2>
+    </Fragment>
+  ) : (
     <Fragment>
       <div className="edit-page-container">
         <h1>Edit recipe</h1>
         <form noValidate autoComplete="off">
           <Box className="edit-container">
-            <Box>Recipe name: {recipeName}</Box>
+            <Box>Recipe name: {oldRecipeName}</Box>
             <NameInput setRecipeName={setRecipeName} />
           </Box>
           <Box className="edit-container">
-            <Box>Small Description: {smallDescription}</Box>
+            <Box>Small Description: {oldSmallDescription}</Box>
             <SmallDescriptionInput setSmallDescription={setSmallDescription} />
           </Box>
           <Box className="textfield-container ingredients-container">
@@ -101,13 +126,11 @@ export default function EditRecipe() {
             />
           </Box>
           <Box className="edit-container">
-            <Box>Servings: {servings}</Box>
+            <Box>Servings: {oldServings}</Box>
             <ServingsInput servings={servings} setServings={setServings} />
           </Box>
           <Box className="edit-container">
-            <Box>
-              Preparation time: {preparationTimeAmount} {preparationTimeUnit}
-            </Box>
+            <Box>Preparation time: {oldPreparationTime}</Box>
             <PreparationTimeInput
               setPreparationTimeAmount={setPreparationTimeAmount}
               preparationTimeUnit={preparationTimeUnit}
@@ -115,9 +138,7 @@ export default function EditRecipe() {
             />
           </Box>
           <Box className="edit-container">
-            <Box>
-              Cook time: {cookTimeAmount} {cookTimeUnit}
-            </Box>
+            <Box>Cook time: {oldCookTime}</Box>
             <CookTimeInput
               setCookTimeAmount={setCookTimeAmount}
               cookTimeUnit={cookTimeUnit}
@@ -125,9 +146,7 @@ export default function EditRecipe() {
             />
           </Box>
           <Box className="edit-container">
-            <Box>
-              Additional time: {additionalTimeAmount} {additionalTimeUnit}
-            </Box>
+            <Box>Additional time: {oldAdditionalTime}</Box>
             <AdditionalTimeInput
               setAdditionalTimeAmount={setAdditionalTimeAmount}
               additionalTimeUnit={additionalTimeUnit}
@@ -139,6 +158,7 @@ export default function EditRecipe() {
               variant="contained"
               id="edit-recipe-btn"
               onClick={tryEditRecipe}
+              color="primary"
             >
               Edit recipe
             </Button>
@@ -148,7 +168,7 @@ export default function EditRecipe() {
           show={show}
           setShow={setShow}
           url={`/recipes/${recipeId}`}
-          message="Your recipe has been added successfully"
+          message="Your recipe has been edited successfully"
         />
       </div>
     </Fragment>
